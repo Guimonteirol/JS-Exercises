8f3c7d9e2f9d1439f6ea66785a9a0498a9d28295
Guilherme Monteiro <gui.gjacobina@gmail.com>
2022-03-24 09:59:19 -0300

    update

--- URI/1001.js
+++
@@ -1,7 +0,0 @@
-var input = require("fs").readFileSync("teste", "utf8");
-var lines = input.split('\n');
-
-var A = Number(lines.shift());
-var B = Number(lines.shift());
-
-console.log(`X = ${A+B}`);
--- URI/1005.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('teste', 'utf8');
-var lines = input.split('\n');
-
-let A = Number(lines.shift());
-let B = Number(lines.shift());
-
-console.log(`MEDIA = ${(((A*3.5)+(B*7.5))/11).toFixed(5)}`)
